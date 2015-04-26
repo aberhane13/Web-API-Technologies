9b3ee9c2bc970e2d82d0d22968b99d494d1accd7
aberhane13 <abel.berhane13@gmail.com>
2015-04-25 23:58:11 -0600

    hw 5 submission

--- hw5/apiproxy/resources/node/server-usergrid.js
+++ hw5/apiproxy/resources/node/server-usergrid.js
@@ -2,73 +2,67 @@ var usergrid = require('usergrid');
 var express = require('express');
 var app = express();
 
-var myClient = new usergrid.client({
+var client = new usergrid.client({
     orgName:'aberhane13',
     appName:'sandbox'
 });
 
-
-//Retrieve specific entity
-app.all('/gets', function(req, res) {
+app.all('/gets', function(req, res){
   
+  var properties = {  
+	type:'movies',
+    name: req.query.title,
+
+}; 
   
-	var connecting_entity_options = {
-	    client: myClient,
-	    data: {
-    	    type:'reviews',
-	        //username:'aberhane13',
-    	  	title: req.query.title
-   		 }
-	};
-	var connecting_entity = new usergrid.entity(connecting_entity_options);
-
-	// create an entity object that models the entity being connected to
-	var connected_entity_options = {
-	    client: myClient,
-	    data: {
-	        type:'movies',
-	        //username:'aberhane13',
-	      	title: req.query.title
- 	   }
-	};
-	var connected_entity = new usergrid.entity(connected_entity_options); 
-
-	// the connection type
-	var relationship = 'likes';
-
-	//If reviews query equals true, connect reviews entity with movie entitity
-	if(req.query.reviews == "true")
-	{
-		// send the POST request
-		connecting_entity.connect(relationship, connected_entity, function (error, result) {
-
-		if (error) { 
-        // Error
-		} 
-          else { 
-		// Success
-		}
-
-		});
+  var properties2 = {
+    type: 'reviews',
+    name: req.query.title
+  };
   
-	}
-	var relationship2 = 'connections';
-	connected_entity.getConnections(relationship2, function (error, result) {
+  client.getEntity(properties, function(err, existingUser){
+  	if (err){
       
-		if (error) { 
-        	// Error
-     		res.send("error");
-    	} 
-      else { 
-      	// Success
-      	res.send(result);
-      }
+      res.send("Cannot get first entity");
+      
+        
+    } 
+    else {
+      
+      client.getEntity(properties2, function(err2, existingUser2){
+        if(err2){
+          res.send("Cannot get second entity");
+        }
+      	else{
+          
+          //Make connection if query string reviews = true
+          if(req.query.reviews == "true")
+          {
+      		existingUser.connect('connections', existingUser2, function (err3, data) {
+    			if (err3) {
+    			 // Error - connection not created
+    		 	 res.send("Cannot connect");
+   		 		} 
+            	else {
+				
+                 res.send(data);
 
-	});
+   			 	}
+      		});
+          }
+          
+          //Display the entity without the connection
+          else{
+            res.send(existingUser);
+          }
+         
+    }
+
   
- 
+	});
+    }
+  });
 });
-
 
 app.listen(3000);
 
